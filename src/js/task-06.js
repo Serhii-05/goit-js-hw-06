33add9b30ccff895ae3ea62e1cb0d2cebc481c56
Serhii Melnychenko <higankingpro@gmail.com>
2022-02-07 12:19:52 +0200

    tasks 6

---
+++ src/js/task-06.js
@@ -0,0 +1,22 @@
+const inputEL = document.querySelector('#validation-input');
+const attributeValue = inputEL.getAttribute('data-length');
+const maxInputlenght = parseInt(attributeValue, 10);
+
+inputEL.addEventListener('blur', inputBlur);
+
+function inputBlur() {
+    if (inputEL.value.length === maxInputlenght) {
+        inputEL.classList.add("valid");
+        inputEL.classList.remove("invalid");  
+    }
+    
+    if (inputEL.value.length !== maxInputlenght) {
+        inputEL.classList.add("invalid");
+        inputEL.classList.remove("valid");
+    }
+    
+    if (inputEL.value.length === 0) {
+        inputEL.classList.remove("valid");
+        inputEL.classList.remove("invalid");
+    }
+};
