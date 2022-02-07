09b8e01c4b7b1fc96df34960a9c2a7bac7ab69c7
Serhii Melnychenko <higankingpro@gmail.com>
2022-02-07 22:23:59 +0200

    tasks 8 and 9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,19 +1,22 @@
-// Напиши скрипт управления формой логина.
-
-// 1 Обработка отправки формы form.login-form должна быть по событию submit.
-
-// 2 При отправке формы страница не должна перезагружаться.
-
-// 3 Если в форме есть незаполненные поля, выводи alert с предупреждением о том,
-//   что все поля должны быть заполнены.
-
-// 4 Если пользователь заполнил все поля и отправил форму, собери значения полей
-//   в    обьект, где имя поля будет именем свойства, а значение поля - значением свойства.
-
-// 5 Для доступа к элементам формы используй свойство elements.
-
-// 6 Выведи обьект с введенными данными в консоль и очисти значения полей
-//   формы   методом reset.
+const formRef = document.querySelector('.login-form')
 
 
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(ev) {
+    ev.preventDefault();
+    
+  const formElements = ev.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+    const formData = { email, password };
+    
+  if (email && password) {
+    console.log(formData)
+      } else {
+      alert('Пожалуйста, заполните все поля')
+      
+    }
+    ev.currentTarget.reset();
+  }
 
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', bodyColorChenge);
+
+function bodyColorChenge() {
+  const newColor = getRandomHexColor();
+  bodyEl.style.background = newColor;
+  spanEl.textContent = newColor;
+}
